caa72cd36272977a7cf76dee1cddf8e4926b8c57
Eric Craft <eacraft13@gmail.com>
2015-12-17 02:33:32 -0800

    Make executable

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+#!/usr/bin/env node
 var config = require('./config');
 var covers = require('./bin/covers');
 
 
 
-setInterval(function() {
-    covers(config, function(err) {
-        if (err) console.log(err);
-        else console.log('Successfully scraped covers...');
-    });
-}, config.timer);
+covers(config, function(err) {
+    if (err) console.log(err);
+    else console.log('Successfully scraped covers...');
+});
